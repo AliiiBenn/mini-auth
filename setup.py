f8cf139e3d8c94a6e0015dae01d6946180c4a9c1
AliiiBenn <davidddpereiraaa6@gmail.com>
2025-04-17 23:22:14 +0200

    Update

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="mini-auth",
-    version="1.0.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    install_requires=[
-        "fastapi>=0.104.1",
-        "uvicorn>=0.24.0",
-        "sqlalchemy>=2.0.23",
-        "aiosqlite>=0.19.0",
-        "alembic>=1.12.1",
-        "python-jose>=3.3.0",
-        "python-multipart>=0.0.6",
-        "pydantic>=2.5.2",
-        "pydantic-settings>=2.1.0",
-        "python-dotenv>=1.0.0",
-        "email_validator>=2.2.0",
-        "passlib>=1.7.4",
-        "bcrypt>=4.0.1"
-    ],
-    python_requires=">=3.8",
-) 
+setup() 
